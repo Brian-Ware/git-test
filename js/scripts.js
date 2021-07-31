848f550a7b2e725aa562ece8a1e4d6443c0faffd
Brian Ware <gmontag25@gmail.com>
2021-07-31 12:07:01 -0400

    Week 4 assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,29 @@
 
-    $(function () {
-        $(".carousel").carousel({interval: 2000});
-        $("#carouselButton").click(function () {
-            if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-                $(".carousel").carousel("pause");
-                $("#carouselButton").children("i").removeClass("fa-pause");
-                $("#carouselButton").children("i").addClass("fa-play");
-            }else{
-                $(".carousel").carousel("cycle");
-                $("#carouselButton").children("i").removeClass("fa-play");
-                $("#carouselButton").children("i").addClass("fa-pause")
-            }
-        });
+$(function () {
+    $(".carousel").carousel({interval: 2000});
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        }else{
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause")
+        }
     });
+    $("#reserveButton").click(function(){
+        console.log("reserve")
+        $("#reserveModal").modal("show");
+    });
+    $("#loginButton").click(function(){
+        console.log("reserve")
+        $("#loginModal").modal("show");
+    });
+
+});
+
+
+ 
+
+  
